fa4da6db5571dbeb49c68d71b92827a4d5a05a6e
vermaayush680 <vermaayush680@gmail.com>
2022-04-06 01:09:14 +0530

    Added Loader and Icon

--- predictions.py
+++ predictions.py
@@ -1,6 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
+import pandas as pd
 from tensorflow import keras
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
@@ -8,14 +9,15 @@ from keras.preprocessing import image
 from PIL import Image
 import streamlit as st
 
+#Function to load uploaded image
 def load_image(image_file):
 	img = Image.open(image_file)
 	return img
-
-def check(img):
+# Function to check the image
+def check():
 
     lr = keras.models.load_model('weights.h5')
-    print('loaded')
+    #Prediction Pipeline
     class Preprocessor(BaseEstimator, TransformerMixin):
         def fit(self,img_object):
             return self
@@ -36,31 +38,37 @@ def check(img):
 
     full_pipeline = Pipeline([('preprocessor',Preprocessor()),
                             ('predictor',Predictor())])
+    return full_pipeline
 
-    a=  img
-    #a = img.decode('utf-8', 'ignore') 
-    a= a.resize((256,256))
-    predic = full_pipeline.predict(a)
-    return(predic)
+def output(full_pipeline,img):
+   a=  img
+   #a = img.decode('utf-8', 'ignore') 
+   a= a.resize((256,256))
+   predic = full_pipeline.predict(a)
+   return(predic)
 
 def main():
-    # giving a title
-    st.title('African Wildlife Animal Classifier')
-    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-    # code for Prediction
-    prediction = ''
+   # giving a title
+   st.set_page_config(page_title='African Wildlife', page_icon='favicon.png')
+   st.title('African Wildlife Animal Classifier')
+   st.subheader('Upload either Buffalo/Elephant/Rhino/Zebra image for prediction')
+   image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+   # code for Prediction
+   prediction = ''
 
-    # creating a button for Prediction
-    
-    if st.button('Predict'):
-        if image_file is not None:
-            # To See details
-            file_details = {"filename":image_file.name, "filetype":image_file.type,"filesize":image_file.size}
-            st.write(file_details)
-            img = load_image(image_file)
-            st.image(img,width=256)
-            prediction = check(img)
-            
-        st.success(prediction)
+   # creating a button for Prediction
+
+   if st.button('Predict'):
+     if image_file is not None:
+         # To See details
+         with st.spinner('Loading Image and Model...'):
+            full_pipeline = check()
+         file_details = {"filename":image_file.name, "filetype":image_file.type,"filesize":image_file.size}
+         st.write(file_details)
+         img = load_image(image_file)
+         st.image(img,width=256)
+         with st.spinner('Predicting...'):
+            prediction = output(full_pipeline,img)
+         st.success(prediction)
 if __name__ == '__main__':
     main()
